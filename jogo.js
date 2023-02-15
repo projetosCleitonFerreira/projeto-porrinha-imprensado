38c933a2b7e64b7cbc854484760596c3d2fba3eb
Cleiton de Santana Ferreira <103061676+projetosCleitonFerreira@users.noreply.github.com>
2023-02-15 01:38:11 -0300

    Melhorei a lógica

--- jogo.js
+++ jogo.js
@@ -1,14 +1,16 @@
+//var hora = new Date().getHours();
+
 let numeroSecreto = parseInt(Math.random() * 101);
 let lista = document.querySelector('select#flista')
 let chute = window.document.getElementById('fnum');
 let valores = []
 
 function isNumero(n) {
-    if(Number(n) >= 0 && Number(n) <= 100) {
+    if(Number(n) > 0 && Number(n) < 100) {
         return true
     } else {
-        return false
-    }
+        return false 
+    }   
 }
 
 function inLista(n, l) {
@@ -19,25 +21,27 @@ function inLista(n, l) {
     }
 }
 
+
 function tentar() {
     var valor = Number(chute.value)
- 
+    
     if (isNumero(valor) && !inLista(valor, valores)) {
         valores.push(valor)
         let item = document.createElement('option')
         item.text = `O valor adicionado foi ${valor}.`
         lista.appendChild(item)
-    } else {
+        } else {
         window.alert('Número inválido ou já adicionado!')
         } if (valor == numeroSecreto) {
             res.innerHTML = 'Acertou!'
-        } else if (valor > numeroSecreto) {
+        } else if (valor > numeroSecreto && valor <= 99) {
             res.innerHTML = 'Errou... o número secreto é menor!'
-        } else if (valor < numeroSecreto) {
+        } else if (valor < numeroSecreto && valor >=1) {
             res.innerHTML = 'Errou... o número secreto é maior!'
         } else {
             res.innerHTML = 'Adicione um número!!'
-        }
+    }   
+    
     chute.value = ''  
     chute.focus()
 }
